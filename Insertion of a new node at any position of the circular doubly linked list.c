82c1dffee271830344e93fdc4cdf3f0ded7d7154
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-08 19:22:46 +0530

    Create Insertion of a new node at any position of the circular doubly linked list.c

---
+++ Insertion of a new node at any position of the circular doubly linked list.c
@@ -0,0 +1,147 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+	struct node *prev;
+}*head,*b;
+
+void create(int n);
+void display();
+void insertatanypos(int value,int pos);
+
+int main()
+{
+	int n,value,pos;
+	
+	printf("Enter the number of nodes:");
+	scanf("%d",&n);
+	create(n);
+	
+	printf("Display the list:\n");
+	display();
+	
+	printf("\nEnter the position from user:");
+	scanf("%d",&pos);
+	printf("Enter the value from user:");
+	scanf("%d",&value);
+	insertatanypos(value,pos);
+	
+	printf("Display the list:\n");
+	display();
+  
+	return 0;
+}
+
+void create(int n)
+{
+	struct node *newnode;
+	int value;
+	head=(struct node *)malloc(sizeof(struct node));
+	if(head==NULL)
+	{
+		printf("Memory allocation not possible");
+	}
+	else
+	{
+		printf("Enter the value of node 1:");
+		scanf("%d",&value);
+		head->data=value;
+		head->next=NULL;
+		head->prev=NULL;
+		b=head;
+	}
+	for(int i=2;i<=n;i++)
+	{
+		newnode=(struct node *)malloc(sizeof(struct node));
+		if(newnode==NULL)
+		{
+			printf("Memory allocation not possible");
+		}
+		else
+		{
+			printf("Enter the value of node %d:",i);
+			scanf("%d",&value);
+			newnode->data=value;
+			newnode->next=NULL;
+			newnode->prev=b;
+			b->next=newnode;
+			b=b->next;
+		}
+	}
+	/*creation of circular doubly linked list,all codes above are same as that
+	of the creation of doubly linked list but the below two lines of code 
+	creates the circular doubly linked list*/
+	
+	b->next=head;//linking the last node with the first node or head node
+	head->prev=b;//linking the head node with the last node
+	
+}
+
+void display()
+{
+	int i=1;
+	struct node *temp;
+	if(head==NULL)
+	{
+		printf("List is empty");
+	}
+	else
+	{
+		temp=head;
+		while(temp->next!=head)
+		{
+			printf("Data of %d=%d\n",i,temp->data);
+			temp=temp->next;
+			i++;
+		}
+		printf("Data of %d=%d",i,temp->data);
+	}
+}
+
+void insertatanypos(int value,int pos)
+{
+	head->prev=NULL;
+	b->next=NULL;
+	int i=1;
+	struct node *temp,*newnode;
+	temp=head;
+	if(pos==1)//insertion of a new node in the first position 
+	{
+	    newnode=(struct node *)malloc(sizeof(struct node));
+	    newnode->data=value;
+	    newnode->next=temp;
+	    temp->prev=newnode;
+	    newnode->prev=NULL;
+	    head=newnode;
+	}
+	else if(pos>1)//insertion of a new node in a position greater than 1
+	{
+		while(i<pos-1 && temp!=NULL)
+		{
+			temp=temp->next;
+			i++;
+		}
+		if(temp!=NULL)
+		{
+			newnode=(struct node *)malloc(sizeof(struct node));
+			newnode->data=value;//assigning the value for the first node
+			newnode->next=temp->next;//the link between the new node and the node next to the temp node 
+			newnode->prev=temp;//the new node is further linked with the temp node
+			if(temp->next!=NULL)
+			{
+				temp->next->prev=newnode;//the node next to the temp node is linked with the newnode
+			}
+			temp->next=newnode;//the temp node is linked with the new node
+		}
+	}
+	else
+	{
+		printf("You entered wrong position..!\n");
+	}
+	/*circular list is created*/
+	b->next=head;
+	head->prev=b;
+}
