88fbc4037f9d66a4ebbcda5e3aab6208373d353b
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-29 23:40:44 +0530

    Create Removing all nodes having -1 in the data part.c

---
+++ circular doubly linked list/Removing all nodes having -1 in the data part.c
@@ -0,0 +1,126 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *prev;
+	struct node *next;
+}*head,*end;
+
+void create(int n);
+void display();
+void remove();
+int n;
+
+int main()
+{
+	printf("Enter the number of nodes:");
+	scanf("%d",&n);
+	create(n);
+	printf("\nDisplay the list:\n");
+	display();
+	remove();
+	printf("\n\nDisplay the list after deleting all nodes having value -1:\n");
+	display();
+	return 0;
+}
+void create(int n)
+{
+	int val;
+	struct node *temp;
+	head=(struct node *)malloc(sizeof(struct node));
+	if(head==NULL)
+	{
+		printf("\nMEMORY ALLOCATION NOT POSSIBLE");
+	}
+	else
+	{
+		printf("Enter the value of node 1:");
+		scanf("%d",&val);
+		head->data=val;
+		head->next=NULL;
+		head->prev=NULL;
+		temp=head;
+	}
+	for(int i=2;i<=n;i++)
+	{
+		struct node *newnode;
+		newnode=(struct node *)malloc(sizeof(struct node));
+		if(newnode==NULL)
+		{
+			printf("\nMEMORY ALLOCATION NOT POSSIBLE");
+		}
+		else
+		{
+			printf("Enter the value of node %d:",i);
+			scanf("%d",&val);
+			newnode->data=val;
+			newnode->next=NULL;
+			newnode->prev=temp;
+			temp->next=newnode;
+			temp=temp->next;
+		}
+	}
+	end=temp;
+	end->next=head;
+	head->prev=end;
+}
+void display()
+{
+	struct node *temp;
+	temp=head;
+	int i=1;
+	if(head==NULL)
+	{
+		printf("\nLIST IS EMPTY");
+	}
+	else
+	{
+		while(temp->next!=head)
+		{
+			printf("Data of %d:%d\n",i,temp->data);
+			temp=temp->next;
+			i++;
+		}
+	printf("Data of %d:%d",i,temp->data);
+	}
+}
+void remove()
+{
+	end->next=NULL;
+	head->prev=NULL;
+	struct node *temp;
+	temp=head;
+	while(temp!=NULL)
+	{
+		if(head->data==-1)
+		{
+			struct node *b;
+			b=head;
+			head=head->next;
+			free(b);
+		}
+		else if(end->data==-1)
+		{
+			struct node *c;
+			c=end;
+			end=end->prev;
+			free(c);
+		}
+		else
+		{
+			if(temp->data==-1)
+			{
+			struct node *a;
+			a=temp;
+			temp->next->prev=temp->prev;
+			temp->prev->next=temp->next;
+			free(a);
+			}
+		}
+	temp=temp->next;
+	}
+	end->next=head;
+	head->prev=end;
+}
