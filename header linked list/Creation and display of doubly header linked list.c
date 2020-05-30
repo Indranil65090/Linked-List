fb15a839aae7b8654d05830aff2783592c59172a
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-30 20:43:00 +0530

    Create Creation and display of doubly header linked list.c

---
+++ header linked list/Creation and display of doubly header linked list.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *next;
+	struct node *prev;
+}*head,*start;
+void create(int n);
+void display();
+int main()
+{
+	int n;
+	start=(struct node *)malloc(sizeof(struct node));
+	if(start==NULL)
+	{
+		printf("\nMEMORY ALLOCATION NOT POSSIBLE");
+	}
+	else
+	{
+		printf("Enter the number of nodes:");
+		scanf("%d",&n);
+		start->data=n;
+		start->next=NULL;
+	}
+	create(n);
+	printf("\nDisplay the list:");
+	display();
+	return 0;
+}
+void create(int n)
+{
+	struct node *temp,*newnode;
+	int value;
+	head=(struct node *)malloc(sizeof(struct node));
+	if(head==NULL)
+	{
+		printf("\nOVERFLOW");
+	}
+	else
+	{
+		printf("Enter the value of node 1:");
+		scanf("%d",&value);
+		head->data=value;
+		head->next=NULL;
+		head->prev=NULL;
+		start->next=head;
+		temp=head;
+	}
+	for(int i=2;i<=n;i++)
+	{
+		newnode=(struct node *)malloc(sizeof(struct node));
+		if(newnode==NULL)
+		{
+			printf("\nOVERFLOW");
+		}
+		else
+		{
+			printf("Enter the value of node %d:",i);
+			scanf("%d",&value);
+			newnode->data=value;
+			newnode->next=NULL;
+			temp->next=newnode;
+			newnode->prev=temp;
+			temp=temp->next;
+		}
+	}
+}
+void display()
+{
+	struct node *temp;
+	temp=head;
+	int i=1;
+	if(head==NULL)
+	{
+		printf("\nLIST IS EMPTY");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			printf("\nData of %d=%d",i,temp->data);
+			temp=temp->next;
+			i++;
+		}
+	}
+}
