24a985e5bb84a1edc89c0b2a39cf2f168d5425bf
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-31 10:27:04 +0530

    Create Inserting a new node at the beginning of the grounded linked list.c

---
+++ header linked list/Inserting a new node at the beginning of the grounded linked list.c
@@ -0,0 +1,127 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *next;
+}*head,*start,*a;
+void create(int n);
+void display();
+void displaynewlist();
+void insertbeg();
+int main()
+{
+	int n;
+	start=(struct node *)malloc(sizeof(struct node));
+	if(start==NULL)
+	{
+		printf("\nMEMORY ALLOCATION NOT POSSIBLE");
+	}
+	else
+	{
+		printf("Enter the number of nodes:");
+		scanf("%d",&n);
+		start->data=n;
+		start->next=NULL;
+	}
+	create(n);
+	printf("\nDisplay the list:");
+	display();
+	insertbeg();
+	printf("\nDisplay the list:");
+	displaynewlist();
+	return 0;
+}
+void create(int n)
+{
+	struct node *temp,*newnode;
+	int value;
+	head=(struct node *)malloc(sizeof(struct node));
+	if(head==NULL)
+	{
+		printf("\nOVERFLOW");
+	}
+	else
+	{
+		printf("Enter the value of node 1:");
+		scanf("%d",&value);
+		head->data=value;
+		head->next=NULL;
+		start->next=head;
+		temp=head;
+	}
+	for(int i=2;i<=n;i++)
+	{
+		newnode=(struct node *)malloc(sizeof(struct node));
+		if(newnode==NULL)
+		{
+			printf("\nOVERFLOW");
+		}
+		else
+		{
+			printf("Enter the value of node %d:",i);
+			scanf("%d",&value);
+			newnode->data=value;
+			newnode->next=NULL;
+			temp->next=newnode;
+			temp=temp->next;
+		}
+	}
+}
+void display()
+{
+	struct node *temp;
+	temp=head;
+	int i=1;
+	if(head==NULL)
+	{
+		printf("\nLIST IS EMPTY");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			printf("\nData of %d=%d",i,temp->data);
+			temp=temp->next;
+			i++;
+			
+		}
+	}
+}
+void insertbeg()
+{
+	int value;
+	a=(struct node *)malloc(sizeof(struct node));
+	if(a==NULL)
+	{
+		printf("\nNO MEMORY TO ALLOCATE");
+	}
+	else
+	{
+		printf("\n\nEnter the value of the new node to be inserted at the beginning:");
+		scanf("%d",&value);
+		a->data=value;
+		start->next=a;
+		a->next=head;
+	}
+}
+void displaynewlist()
+{
+	struct node *temp;
+	temp=a;
+	int i=1;
+	if(a==NULL)
+	{
+		printf("\nLIST IS EMPTY");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			printf("\nData of %d=%d",i,temp->data);
+			temp=temp->next;
+			i++;
+			
+		}
+	}
+}
