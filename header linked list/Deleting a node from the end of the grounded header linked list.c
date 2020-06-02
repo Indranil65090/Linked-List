b86db1e6f80fcc6574e47dc08cf08d3c9cdf7ebd
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-06-02 15:55:53 +0530

    Create Deleting a node from the end of the grounded header linked list.c

---
+++ header linked list/Deleting a node from the end of the grounded header linked list.c
@@ -0,0 +1,123 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *next;
+}*head,*start,*end;
+void create(int n);
+void display();
+void displaynewlist();
+void deleteend();
+int main()
+{
+	int n;
+	start=(struct node *)malloc(sizeof(struct node));
+	if(start==NULL)
+	{
+		printf("\nMEMORY ALLOCATION NOT POSSIBLE");
+	}
+	else
+	{
+		printf("Enter the number of nodes:");
+		scanf("%d",&n);
+		start->data=n;
+		start->next=NULL;
+	}
+	create(n);
+	printf("\nDisplay the list:");
+	display();
+	deleteend();
+	printf("\n\nDisplay the list:");
+	displaynewlist();
+	return 0;
+}
+void create(int n)
+{
+	struct node *temp,*newnode;
+	int value;
+	head=(struct node *)malloc(sizeof(struct node));
+	if(head==NULL)
+	{
+		printf("\nOVERFLOW");
+	}
+	else
+	{
+		printf("Enter the value of node 1:");
+		scanf("%d",&value);
+		head->data=value;
+		head->next=NULL;
+		start->next=head;
+		temp=head;
+	}
+	for(int i=2;i<=n;i++)
+	{
+		newnode=(struct node *)malloc(sizeof(struct node));
+		if(newnode==NULL)
+		{
+			printf("\nOVERFLOW");
+		}
+		else
+		{
+			printf("Enter the value of node %d:",i);
+			scanf("%d",&value);
+			newnode->data=value;
+			newnode->next=NULL;
+			temp->next=newnode;
+			temp=temp->next;
+		}
+		end=temp;
+	}
+	
+}
+void display()
+{
+	struct node *temp;
+	temp=head;
+	int i=1;
+	if(head==NULL)
+	{
+		printf("\nLIST IS EMPTY");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			printf("\nData of %d=%d",i,temp->data);
+			temp=temp->next;
+			i++;
+		}
+	}
+}
+void deleteend()
+{
+	struct node *temp,*a;
+	temp=head;
+	while(temp->next!=end)
+	{
+		temp=temp->next;
+	}
+	a=end;
+	printf("\n\nThe data to be deleted is:%d",a->data);
+	temp->next=NULL;
+	free(a);
+}
+void displaynewlist()
+{
+	struct node *temp;
+	temp=head;
+	int i=1;
+	if(head==NULL)
+	{
+		printf("\nLIST IS EMPTY");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			printf("\nData of %d=%d",i,temp->data);
+			temp=temp->next;
+			i++;
+		}
+	}
+}
