8db7f0e9b0805aa3c6f8702544693c8f6fa16c0f
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-06-05 08:50:22 +0530

    Create Deleting a node from the beginning of the doubly header linked list.c

---
+++ header linked list/Deleting a node from the beginning of the doubly header linked list.c
@@ -0,0 +1,122 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *next;
+	struct node *prev;
+}*head,*start;
+void create(int n);
+void display();
+void deletebeg();
+void displaylist();
+int main()
+{
+	int n;
+	start=(struct node *)malloc(sizeof(struct node));
+	if(start==NULL)
+	{
+		printf("\nMEMORY ALLOCATION NOT POSSIBLE");
+	}
+	else
+	{
+		printf("Enter the number of nodes:");
+		scanf("%d",&n);
+		start->data=n;
+		start->next=NULL;
+	}
+	create(n);
+	printf("\nDisplay the list:");
+	display();
+	deletebeg();
+	printf("\n\nDisplay the list:");
+	displaylist();
+	return 0;
+}
+void create(int n)
+{
+	struct node *temp,*newnode;
+	int value;
+	head=(struct node *)malloc(sizeof(struct node));
+	if(head==NULL)
+	{
+		printf("\nOVERFLOW");
+	}
+	else
+	{
+		printf("Enter the value of node 1:");
+		scanf("%d",&value);
+		head->data=value;
+		head->next=NULL;
+		head->prev=NULL;
+		start->next=head;
+		temp=head;
+	}
+	for(int i=2;i<=n;i++)
+	{
+		newnode=(struct node *)malloc(sizeof(struct node));
+		if(newnode==NULL)
+		{
+			printf("\nOVERFLOW");
+		}
+		else
+		{
+			printf("Enter the value of node %d:",i);
+			scanf("%d",&value);
+			newnode->data=value;
+			newnode->next=NULL;
+			temp->next=newnode;
+			newnode->prev=temp;
+			temp=temp->next;
+		}
+	}
+}
+void display()
+{
+	struct node *temp;
+	temp=head;
+	int i=1;
+	if(head==NULL)
+	{
+		printf("\nLIST IS EMPTY");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			printf("\nData of %d=%d",i,temp->data);
+			temp=temp->next;
+			i++;
+		}
+	}
+}
+void deletebeg()
+{
+	struct node *temp,*a;
+	temp=start;
+	a=head;
+	head=head->next;
+	temp->next=head;
+	head->prev=NULL;
+	printf("\n\nThe data to be deleted is:%d",a->data);
+	free(a);
+}
+void displaylist()
+{
+	struct node *temp;
+	temp=head;
+	int i=1;
+	if(head==NULL)
+	{
+		printf("\nLIST IS EMPTY");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			printf("\nData of %d=%d",i,temp->data);
+			temp=temp->next;
+			i++;
+		}
+	}
+}
