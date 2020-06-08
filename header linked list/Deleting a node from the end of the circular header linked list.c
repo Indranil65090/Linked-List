9aedced62d61e82eb4610da7be78ec9a008a09a4
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-06-08 08:15:22 +0530

    Create Deleting a node from the end of the circular header linked list.c

---
+++ header linked list/Deleting a node from the end of the circular header linked list.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+}*head,*start,*end;
+
+void create(int n);
+void display();
+void deleteend();
+int n;
+
+int main()
+{
+	
+	start=(struct node *)malloc(sizeof(struct node));
+	if(start==NULL)
+	{
+		printf("\nMEMORY ALLOCATION NOT POSSIBLE");
+	}
+	else
+	{
+		printf("Enter the number of nodes:");
+		scanf("%d",&n);
+		start->data=n;
+		start->next=NULL;
+	}
+	create(n);
+	printf("\nDisplay the list:");
+	display();
+	deleteend();
+	printf("\n\nDisplay the list:");
+	display();
+	return 0;
+}
+void create(int n)
+{
+	struct node *temp,*newnode;
+	int value;
+	head=(struct node *)malloc(sizeof(struct node));
+	if(head==NULL)
+	{
+		printf("\nOVERFLOW");
+	}
+	else
+	{
+		printf("Enter the value of node 1:");
+		scanf("%d",&value);
+		head->data=value;
+		head->next=NULL;
+		start->next=head;
+		temp=head;
+	}
+	for(int i=2;i<=n;i++)
+	{
+		newnode=(struct node *)malloc(sizeof(struct node));
+		if(newnode==NULL)
+		{
+			printf("\nOVERFLOW");
+		}
+		else
+		{
+			printf("Enter the value of node %d:",i);
+			scanf("%d",&value);
+			newnode->data=value;
+			newnode->next=NULL;
+			temp->next=newnode;
+			temp=temp->next;
+		}
+	}
+	end=temp;
+	end->next=head;
+}
+void display()
+{
+	struct node *temp;
+	temp=head;
+	int i=1;
+	if(head==NULL)
+	{
+		printf("\nLIST IS EMPTY");
+	}
+	else
+	{
+		while(temp->next!=head)
+		{
+			printf("\nData of %d=%d",i,temp->data);
+			temp=temp->next;
+			i++;
+		}
+		printf("\nData of %d=%d",i,temp->data);
+	}
+}
+void deleteend()
+{
+	struct node *temp,*temp1;
+	temp=head;
+	temp1=end;
+	if(temp==NULL)
+	{
+		printf("LIST IS EMPTY");
+	}
+	else
+	{
+		end->next=NULL;
+		while(temp->next!=end)
+		{
+			temp=temp->next;
+		}
+		temp->next=head;
+		printf("\n\nThe data to be deleted is:%d",temp1->data);
+		free(temp1);
+	}
+}
